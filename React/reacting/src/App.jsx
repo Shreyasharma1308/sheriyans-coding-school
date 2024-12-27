4fe016133cb2c3fab7713910fefb26f73b04d188
Shreyasharma1308 <Shreyasharma130815@gmail.com>
2024-12-27 14:03:49 +0530

    component resuability

---
+++ React/reacting/src/App.jsx
@@ -0,0 +1,11 @@
+ import React from "react";
+ import Card from "./Components/Card";
+ function App(){
+  const data = ["harsg", "harsh sharma","prachi"];
+  return (
+    <div>
+      <Card />
+    </div>
+  );
+ }
+ export default App;
---
+++ React/reacting/src/Components/Card.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Card() {
+  const data=[{image:'https://plus.unsplash.com/premium_photo-1728044138824-be1f80079bfc?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw2fHx8ZW58MHx8fHx8',name: "2025",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis veniam ratione velit cupiditate, tempora"},
+    {image:'https://images.unsplash.com/photo-1734366965582-1f5bc9d4e21a?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8',name: "a girl",description:"lorem1Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis veniam "},
+    {image:'https://images.unsplash.com/photo-1732452792153-6d244db8dc01?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw5fHx8ZW58MHx8fHx8',name: "scenary",description:"Lorem ipsum dolor sit amet consectetur adipisicing elit."}
+  ]
+  return (
+    <div className='w-full h-screen flex items-center justify-center gap-10 bg-zinc-200'>
+       {
+        data.map((elem,index)=>(
+          <div className='w-52 bg-zinc-100  rounded-md overflow-hidden'>
+          <div className='w-full h-32 bg-zinc-300'>
+             <img className='w-full h-full object-cover' src={elem.image} />
+          </div>
+          <div className='w-full h-full'>
+          <h2 className='font-semibold'>{elem.name}</h2>
+          <p className='text-xs mt-5'>{elem.description}</p>
+          </div>
+         </div>
+        ))
+       }
+           
+    </div>
+  )
+}
+
+export default Card
