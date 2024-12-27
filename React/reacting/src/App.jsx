fef870ee9493889f8be3d6c339030e5372e973e0
Shreyasharma1308 <Shreyasharma130815@gmail.com>
2024-12-27 19:26:11 +0530

    use of useState

--- React/reacting/src/App.jsx
+++ React/reacting/src/App.jsx
@@ -1,11 +1,13 @@
- import React from "react";
+ import React, { useState } from "react";
  import Card from "./Components/Card";
  function App(){
-  const data = ["harsg", "harsh sharma","prachi"];
+  const[val,setVal]=useState(12);
+
   return (
-    <div>
-      <Card />
+    <div className="p-4">
+     <h2>{val}</h2>
+     <button onClick={()=>setVal((prev)=>prev+1)} className="px-4 py-1 bg-blue-400 text-white">change</button>
     </div>
   );
  }
- export default App;
+ export default App; 
